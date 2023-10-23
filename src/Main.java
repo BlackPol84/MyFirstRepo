80c804808f5c7e243b7d5b39594360055d97a94f
BlackPol84 <orlovpol84@gmail.com>
2023-10-23 19:17:16 +0300

    Изменил Main.java и добавил StringUtils

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,8 @@
+import utils.StringUtils;
+
 public class Main {
-    public String reverse (String str) {
-
-        int start = 0;
-        int end = str.length() - 1;
-        char[] output = str.toCharArray();
-
-        for (int i = 0; i < str.length(); i++) {
-
-            if (Character.isLetter(output[start]) && Character.isLetter(output[end])) {
-
-                char temp = output[start];
-                output[start] = output[end];
-                output[end] = temp;
-
-                start++;
-                end--;
-            }
-            else if (!Character.isLetter(output[start]) && !Character.isLetter(output[end])) {
-                start++;
-                end--;
-            }
-            else if (Character.isLetter(output[start]) && !Character.isLetter(output[end])) {
-                end--;
-            }
-            else if (!Character.isLetter(output[start]) && Character.isLetter(output[end])) {
-                start++;
-            }
-            if(start >= end) {
-                return new String(output);
-            }
-        }
-        throw new IllegalArgumentException("Incorrect reverse string");
-    }
-
     public static void main(String[] args) {
-        Main text = new Main();
+        StringUtils text = new StringUtils();
         System.out.println(text.reverse("J@va the be$t!123"));
     }
 }
--- src/utils/StringUtils.java
+++ src/utils/StringUtils.java
@@ -1,2 +1,32 @@
-package utils;public class StringUtils {
+package utils;
+
+public final class StringUtils {
+    public static String reverse (String str) {
+
+        int start = 0;
+        int end = str.length() - 1;
+        char[] output = str.toCharArray();
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if(start >= end) {
+                break;
+            } else if (Character.isLetter(output[start]) && Character.isLetter(output[end])) {
+                char temp = output[start];
+                output[start] = output[end];
+                output[end] = temp;
+                start++;
+                end--;
+
+            } else if (!Character.isLetter(output[start]) && !Character.isLetter(output[end])) {
+                start++;
+                end--;
+            } else if (Character.isLetter(output[start]) && !Character.isLetter(output[end])) {
+                end--;
+            } else if (!Character.isLetter(output[start]) && Character.isLetter(output[end])) {
+                start++;
+            }
+        }
+        return new String(output);
+    }
 }
