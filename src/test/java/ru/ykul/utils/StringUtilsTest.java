32d2615dbcf0fc40d95934d702ac64a8ab32e45f
BlackPol84 <orlovpol84@gmail.com>
2023-10-26 20:19:14 +0300

    Исправил тесты с четной/нечетной длиной строки и исправил замечания с оформлении

--- src/test/java/ru/ykul/utils/StringUtilsTest.java
+++ src/test/java/ru/ykul/utils/StringUtilsTest.java
@@ -6,11 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
     @Test
-    void revers_ifContainsWord_shouldReturnReversString() {
-        assertEquals("l5ooc4", StringUtils.reverse("c5ool4"));
+    void reverse_ifContainsOneNonLetterCharacter_shouldReturnString() {
+        assertEquals("5", StringUtils.reverse("5"));
     }
     @Test
-    void revers_ifContainsEmpty_shouldReturnEmpty () {
+    void reverse_ifContainsEmpty_shouldReturnEmpty () {
         assertEquals("", StringUtils.reverse(""));
     }
     @Test
@@ -18,16 +18,16 @@ class StringUtilsTest {
         assertEquals("x", StringUtils.reverse("x"));
     }
     @Test
-    void reverse_ifContainsNonLettersOnly_returnString() {
+    void reverse_ifContainsNonLettersOnly_shouldReturnString() {
         assertEquals("123@4&!90", StringUtils.reverse("123@4&!90"));
     }
     @Test
-    void reverse_ifContainsEvenNumber_returnString() {
-        assertEquals("4",StringUtils.reverse("4"));
+    void reverse_ifContainsEvenStringLength_shouldReturnReversString() {
+        assertEquals("iLt4ac",StringUtils.reverse("cat4Li"));
     }
     @Test
-    void reverse_ifContainsOddNumber_returnString() {
-        assertEquals("7",StringUtils.reverse("7"));
+    void reverse_ifContainsOddStringLength_shouldReturnReversString() {
+        assertEquals("jo7p$",StringUtils.reverse("po7j$"));
     }
     @Test
     void reverse_ifContainsNull_shouldReturnEmpty() {
